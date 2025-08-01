83c544341f5b6a3c23f2d0812bcf1cd9d5b13496
MariFeitos0 <marifeitos@icloud.com>
2025-07-31 21:27:40 -0300

    Atualiza UI dos botoes

--- init_db.py
+++ init_db.py
@@ -1,22 +1,20 @@
-import sqlite3
+import psycopg2
+import os
 
-# Corrigido: conecta corretamente ao banco na raiz do projeto
-conn = sqlite3.connect('tarefas.db')
+db_url = os.getenv("DATABASE_URL", "postgresql://tarefas_db_aw40_user:ScEtNdQZYgL8Ug0WFJqhcj2bVjfk0u3j@dpg-d26005ali9vc73a16p90-a/tarefas_db_aw40")
+
+conn = psycopg2.connect(db_url)
 cursor = conn.cursor()
 
 cursor.execute('''
-    DROP TABLE IF EXISTS tarefas
-''')
-
-cursor.execute('''
     CREATE TABLE IF NOT EXISTS tarefas (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY,
         conteudo TEXT NOT NULL,
-        concluida INTEGER NOT NULL DEFAULT 0
-    )
+        concluida BOOLEAN DEFAULT FALSE
+    );
 ''')
 
 conn.commit()
+cursor.close()
 conn.close()
-
-print("Banco de dados recriado com sucesso com campo 'concluida'!")
+print("Tabela criada com sucesso.")
